21464f09b89d0232a46eaec4cf8929e259b40b3e
LilyNotFound_ <85715546+LilyWasFound@users.noreply.github.com>
2021-06-25 16:09:00 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,9 +2,7 @@ var car, wall;
 var speed, weight;
 
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-
+  createCanvas(1600,400);
   speed=random(55,90)
   weight=random(400,1500)
 
@@ -15,7 +13,7 @@ function setup() {
 
 function draw() {
   background(255,255,255);  
-
+  drawSprites();
   if(wall.x-car.x < (car.width+wall.width)/2)
   {
     car.velocityX=0;
@@ -34,5 +32,5 @@ function draw() {
       car.shapeColor=color(0,255,0);
     }
   }
-  drawSprites();
+  
 }
